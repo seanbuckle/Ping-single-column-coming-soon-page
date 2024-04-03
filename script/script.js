8e93a25c1d7560590297f324f8f160a6b1bde9c0
seanbuckle <seanlbuckle@gmail.com>
2024-04-03 03:15:26 +0100

    Refactor form validation in script.js

--- script/script.js
+++ script/script.js
@@ -1,22 +1,31 @@
-window.addEventListener('load', () => {
-    let form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
+"use strict";
+window.addEventListener("load", () => {
+    const form = document.getElementById("form");
+    form?.addEventListener("submit", (e) => {
         e.preventDefault();
-        let emailValid = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-        let email = document.getElementById('email');
+        const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+        const email = document.getElementById("email");
         let emailValue = email.value;
-        let errorTxt = document.querySelector('.error-text');
-        if (emailValue == '') {
-            errorTxt.innerHTML = "Whoops! It looks like you forgot to add your email";
+        const errorLabel = document.getElementById("error-label");
+        if (emailValue === "") {
+            if (errorLabel) {
+                errorLabel.textContent =
+                    "Whoops! It looks like you forgot to add your email";
+            }
             email.classList.add("email__error");
             return false;
-        } else if (!emailValue.match(emailValid)) {
-            errorTxt.innerHTML = "Please provide a valid email address";
+        }
+        else if (!emailValue.match(emailRegex)) {
+            if (errorLabel) {
+                errorLabel.textContent = "Please provide a valid email address";
+            }
             email.classList.add("email__error");
             return false;
-
-        } else {
-            errorTxt.innerHTML = "";
+        }
+        else {
+            if (errorLabel) {
+                errorLabel.textContent = "";
+            }
             email.classList.remove("email__error");
         }
     });
