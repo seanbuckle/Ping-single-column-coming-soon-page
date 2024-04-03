89486d16042abf5348de673d67d15f5389e91194
seanbuckle <seanlbuckle@gmail.com>
2024-04-03 03:15:05 +0100

    Add form validation to script.ts

---
+++ script/script.ts
@@ -0,0 +1,31 @@
+window.addEventListener("load", () => {
+  const form = document.getElementById("form") as HTMLFormElement;
+  form?.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+    const email = document.getElementById("email") as HTMLInputElement;
+    let emailValue = email.value;
+    const errorLabel = document.getElementById(
+      "error-label"
+    ) as HTMLLabelElement;
+    if (emailValue === "") {
+      if (errorLabel) {
+        errorLabel.textContent =
+          "Whoops! It looks like you forgot to add your email";
+      }
+      email.classList.add("email__error");
+      return false;
+    } else if (!emailValue.match(emailRegex)) {
+      if (errorLabel) {
+        errorLabel.textContent = "Please provide a valid email address";
+      }
+      email.classList.add("email__error");
+      return false;
+    } else {
+      if (errorLabel) {
+        errorLabel.textContent = "";
+      }
+      email.classList.remove("email__error");
+    }
+  });
+});
